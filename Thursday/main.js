b249637575c9390ef17f140505142dfd25dcc1ca
Timon Rimann <timon.rimann@zhdk.ch>
2025-06-10 09:52:55 +0200

    <<

--- Thursday/main.js
+++ Thursday/main.js
@@ -1,4 +1,5 @@
-// main.js
+// main.js use js for logic, interaction and responsiveness on the website, making them less static. 
+// repeat objects, functions and variables to gain base understanding of js
 console.log("Hello there written once!");
 
 let titleisclicked = false;
@@ -8,19 +9,17 @@ let imgisclicked = false;
 let title= document.getElementById("Test")
 //test if title found
 if(title){
-    console.log("Title found Flo is cool");
+    console.log("Title found:  Flo is cool");
 }
 
-//listening to click here to then change color using this logic
+//listening to click here to then change title color using this logic
 title.addEventListener("click", function() {
     console.log("Title clicked!");
     titleisclicked = !titleisclicked; // Toggle the boolean value
     if(titleisclicked){
     title.style.color = "white"; // Change text color to blue on click
-    title.style.scale = "1"; // Scale the title up on click
     } else{
         title.style.color = "blue"; // Change text color to blue on click
-        title.style.scale = "1.1"; // Scale the title up on click
     }
 })
 
@@ -38,3 +37,20 @@ img.addEventListener("click", function() {
         img.style.transition = "transform 0.5s ease"; // Add a transition effect
     } 
 })
+
+// Function to greet a user with their name and age, two parameters are defined in the ().
+function greet(name, age) {
+    console.log(`Hello ${name}, you are ${age} years old!`);
+}
+
+// Call the function with the different arguments, resulting in a console log output.
+greet("Flo", 30);
+
+
+//query selector all: Will return a list of results, which match the selector
+// let elements = document.querySelectorAll(".imgScaleonclick"); // Select all elements with the class "imgScaleonclick"
+
+//with js you can manipulate the DOM "walk the DOM"
+
+// we also briefly looked APIs (Application Programming Interface) and how they can be used to fetch data from external sources.
+// an API client (e.g. bruno) can help you to debug, simplify and test your API calls.
